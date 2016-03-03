ded1be5e82be6996d6d18fad3f1c07938635cf22
SplittingImage <falcantara@univision.net>
2016-03-02 20:20:12 -0500

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,9 @@
+def display_board
+  print "   |   |   \n"
+  print "-----------\n"
+  print "   |   |   \n"
+  print "-----------\n"
+  print "   |   |   \n"
+end
+
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board

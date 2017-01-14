aef97a9694c9ce6ae3c5ac471b0cfe659c373669
Falgoon Patel <falgoonit@gmail.com>
2017-01-14 04:01:42 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,14 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+def display_board
+  2.times {print "   |"}
+  puts "   "
+  11.times {print "-"}
+  puts ""
+  2.times {print "   |"}
+  puts "   "
+  11.times {print "-"}
+  puts ""
+  2.times {print "   |"}
+  puts "   "
+end

bbee36329977ffb036f32a1f51059b419671d2e1
Harleigh Abel <harleighabel@gmail.com>
2017-03-05 06:29:50 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,18 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+
+  # create string format for rows
+  row = "   |   |   "
+  # create format for lines
+  line = "-----------"
+
+  # print even number of alternating rows and lines
+  2.times do
+    puts row
+    puts line
+  end
+  # print final row
+  puts row
+end
+
+display_board

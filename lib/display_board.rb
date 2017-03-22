7a94c49fcda13337515f1d45621bec6858fa7944
Keith  Polischuk <kpolischuk@gmail.com>
2017-03-22 19:41:34 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,5 @@
+def display_board
+  upright = "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
+puts upright
+end
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board

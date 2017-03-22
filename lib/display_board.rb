9a8ee954119a1ed88b3cb9c26ca148774d3ebe34
Will Sauer <will@heueso.com>
2017-03-22 18:38:28 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,21 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+#display_board prints a blank Board
+def display_board
+#define each of the two elements of the table - rows and dividers
+empty_row = ["   ", "|","   ", "|","   ",]
+table_divider = "-----------"
+
+#first row
+empty_row.each {|x| print x }
+puts
+puts table_divider
+
+#second row
+empty_row.each {|x| print x }
+puts
+puts table_divider
+
+#third row (now divider below it)
+empty_row.each {|x| print x }
+puts
+
+end

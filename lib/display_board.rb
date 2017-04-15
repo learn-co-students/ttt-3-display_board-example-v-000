18f8de37579ff89f887c422c20835a045a8b5eb2
jovon timms <jtim0905@gmail.com>
2017-04-15 14:10:59 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,12 @@
-def display_board = ["   ", "   ", "   ",
-                                     -----------
-                                  "   ", "   ", "   ",
-                                    -----------
-                                  "   ", "   ", "   "]
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+end
+
+
+puts "Welcome to Tic Tac Toe!"
+display_board

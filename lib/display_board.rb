599c159fa99645e0ea2fc8c8cae12755ad845aca
Sally Zasloff <szasloff@gmail.com>
2017-06-28 15:59:54 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 def display_board
-   puts row_1=["   |   |   "]
-   puts row_1a=["-----------"]
-   puts row_2=["   |   |   "]
-   puts row_2a=["-----------"]
-   puts row_3=["   |   |   "]
+   puts "   |   |   "
+   puts "-----------"
+   puts "   |   |   "
+   puts "-----------"
+   puts "   |   |   "
 end
 puts "A Tic Tac Toe Board"
  display_board

f94fa5a8c8790e4a44927fc5f2af5d70fe77a764
JOSELY CASTRO <joselycas@gmail.com>
2017-07-08 20:04:11 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,7 @@
-# Define a medthod display_board that prints a 3x3 Tic Tac Toe Board
-display_board
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end

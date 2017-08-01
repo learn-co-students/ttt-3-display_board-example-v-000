bf35f92adbfd36a6583481e2a95b13e7731bebeb
Allen Anderson <dande313@byu.net>
2017-08-01 18:19:07 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,20 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+BR_LINE = "-----------"
+ROW = ["   ","|","   ","|","   "]
+
+$row_1 = ROW
+$row_2 = ROW
+$row_3 = ROW
+def display_board
+  $row_1.each {|x| print x}
+  puts
+  print BR_LINE
+  puts
+  $row_2.each {|x| print x}
+  puts
+  print BR_LINE
+  puts
+  $row_3.each {|x| print x}
+  puts
+end
+
+display_board

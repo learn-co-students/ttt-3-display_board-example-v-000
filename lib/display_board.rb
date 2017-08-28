0f540c3d24551e8f5f161c606f0f95b1cf4e3ee5
YanChan (Yona) Lin <yonalin1@gmail.com>
2017-08-28 17:48:45 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,9 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  phrase_one = "   ""|""   ""|""   "
+  phrase_two= "-----------"
+  puts phrase_one
+  puts phrase_two
+  puts phrase_one
+  puts phrase_two
+  puts phrase_one
+ end

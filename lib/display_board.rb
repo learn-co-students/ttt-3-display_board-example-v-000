f95151b1aef358fbe264d69f470ab78b2f776bbc
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-09-04 17:52:40 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 def  display_board
-puts row_1 = "   "+"|"+"   "+"|"+"   "
-puts row_2 = "-----------"
-puts row_3 = "   "+"|"+"   "+"|"+"   "
-puts row_4 = "-----------"
-puts row_5 = "   "+"|"+"   "+"|"+"   "
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   "
 end

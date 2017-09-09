d787ef84021602b3b3c4e7e152e21f24ad66fa6f
Judy McGoldrick <jjeanius@verizon.net>
2017-09-09 15:22:06 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,21 +1,9 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
 
 puts "Welcome to Tic Tac Toe"
-board = [ " " , " " , " " , " " , " " , " " , " " , " " , " " ]
-display_board(board)
-
-puts "What move would X like to make (1-9)?"
-puts ">5"
-board = [ " " , " " , " " , " " , "X" , " " , " ", " " , " " ]
-display_board(board)
-
-puts "What move would 0 like to make (1-9)?"
-puts ">1"
-board = [ "0" , " " , " " , " " , "X" , " " , " " , " " , " " ]
-display_board(board)
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end

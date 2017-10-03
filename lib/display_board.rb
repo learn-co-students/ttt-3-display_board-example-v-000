9fc28aefaa2a0f35610aebe81c4c3684ddc1c9b6
Charity Youngblood <youngblood.charity@gmail.com>
2017-10-03 19:31:18 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  $cell =  "   "
+  $vertical_lines = "|"
+  $horizontal_lines = "-----------"
+      2.times do
+      puts $cell + $vertical_lines + $cell + $vertical_lines + $cell
+      puts $horizontal_lines
+    end
+    puts $cell + $vertical_lines + $cell + $vertical_lines + $cell
+  end
+
+display_board

2574f3104189f2c0087002648b8af053bd4dd0c4
Heidi Hlavinka <hchlav@gmail.com>
2017-10-15 21:42:12 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,10 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  puts "   "   "|" "   " "|"   "   "
+  puts "-----------"
+  puts "   "   "|" "   " "|"   "   "
+  puts "-----------"
+  puts "   "   "|" "   " "|"   "   "
+end    # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+#puts row =  ["   ", "|", "   ", "|", "   "] doesn't show quotes or commas, but all on separate new lines
+#puts """ "   "\, | \, "   "\, | \, "   " """ shows commas and bars, not quotes. all on one line.

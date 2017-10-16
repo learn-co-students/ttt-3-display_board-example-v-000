d1a1e06e45da98002e5e5e6f3f7eb1d4cb311829
Halie Kastl <haliekastl@gmail.com>
2017-10-16 18:36:39 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,16 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+# Define a method display_board that prints a 3x3 Tic Tac Toe Boa
+# **Board Rules**
+#
+# 1. Each cell is presented by a string with 3 spaces: <pre>"   "</pre>
+# 2. Each row has 3 cells, the middle separated by 2 `|` (pipe) characters: <pre>   |   |   </pre>
+# 3. There are 3 rows, with 2 separating lines of 11 `-` (dash) characters: `-----------`
+#
+# You can build a single big string and output it once or you can output each line individually. The end result when you execute your method should be the ascii characters above.
+
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end

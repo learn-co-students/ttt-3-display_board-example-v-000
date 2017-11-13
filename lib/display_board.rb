1e63ab2bdac71ba3f3365e44995a10cb05411d0e
James Dolan <jwdolan820@gmail.com>
2017-11-13 00:43:10 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,4 +6,7 @@ def display_board
   puts "   " + "|   |" + "   "
 end
 
-display_board
+
+#each cell is presented by a string with 3 spaces
+#each row has 3 cells, the middle separated by 2 | characters
+#there are 3 rows, with 2 separating lines of 11 - characters

811c038c8ad665b09204f8e28d8e4778192947c6
Daniel Gormley <djg3396@gmail.com>
2018-02-06 04:09:21 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-   |  |
-  ------
-   |  |
-  ------
-   |  |
+  phrase = "display_board"
+ 1.times{print "    |   |    "}
+ 1.times{print " ----------- "}
+ 1.times{print "    |   |    "}
+ 1.times{print " ----------- "}
+ 1.times{print "    |   |    "}
 end

8727c2f3d4901c53376a4751e7d44c178a0b3cda
Karen O <k.ortiz026@gmail.com>
2018-02-23 00:24:02 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,31 +1,40 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  board_emptyline = ["   ","|", "   ", "|" , "   "];
-  board_dashes = ["-"]
+  #Holds values for 9 spaces on the board
+  board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ","   ", "   "]
 
   #prints first line of board
-  board_emptyline.each do |num|
-    print num
+  (0..2).each do |i|
+    print board[i]
+    if i < 2
+      print "|"
+    end
   end
   puts "\n"
 
   #prints horizontal line between line 1 and 2 on board
-  10.times {print board_dashes[0]}
-  puts board_dashes[0]
+  11.times {print "-"}
+  puts "\n"
 
   #prints second line of board
-  board_emptyline.each do |num|
-    print num
+  (3..5).each do |i|
+    print board[i]
+    if i < 5
+      print "|"
+    end
   end
   puts "\n"
 
   #prints horizontal line between line 2 and 3 on board
-  10.times {print board_dashes[0]}
-  puts board_dashes[0]
+  11.times {print "-"}
+  puts "\n"
 
   #prints third line of board
-  board_emptyline.each do |num|
-    print num
+  (6..8).each do |i|
+    print board[i]
+    if i < 8
+      print "|"
+    end
   end
-  puts "\n"
+  print "\n"
 end

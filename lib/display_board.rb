2079ad6e2e7830b509d7a735ef37743c9fade024
Jonathan Williams <jtw365@live.missouristate.edu>
2018-02-28 00:31:20 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,15 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+# Define a method display_board that prints a 3x3 Tic Tac Toe Boar
+def row
+  ["   " "|" "   " "|" "   "]
+end
+
+
+
+def display_board
+  dash = ["-----------"]
+  puts row
+  puts dash
+  puts row
+  puts dash
+  puts row
+end

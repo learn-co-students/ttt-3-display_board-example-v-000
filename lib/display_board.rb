9278e2f842c7168b181b6340408ffe67cb857c23
Carrie Savinell <carrielane89@gmail.com>
2018-03-15 18:08:18 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,17 @@
-def display_board
-  puts "A Tic Tac Toe Board"
-end
+def 
+  display_board
+   cell = "   "
+   separator = "|"
+   lines = "-----------"
+   
+puts #{cell}#{separator}#{cell}
+puts #{lines}
+puts #{cell}#{separator}#{cell}
+puts #{lines}
+puts #{cell}#{separator}#{cell}
+end 
 
-display_board
-A Tic Tac Toe Board
+
+
+
   

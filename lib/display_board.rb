effccc97fd05e0b957a5365dc15e3a831bee1037
geoffrey woodcock <theatomproject@gmail.com>
2018-03-27 19:16:26 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  cell = "   " + "|" + "   " + "|" + "   "
+  line = "-----------"
+puts cell
+puts line
+puts cell
+puts line
+puts cell
+end
+

669cdf042b2bde7949eab1340cdcbbb39eba091e
Taylor Williams <taylor@thewilliams.net>
2018-05-01 22:57:56 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,15 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  def display_row
+    empty_board_space = "   "
+    "#{empty_board_space}|#{empty_board_space}|#{empty_board_space}"
+  end
+  def display_row_separation
+    "-----------"
+  end
+  puts display_row
+  puts display_row_separation
+  puts display_row
+  puts display_row_separation
+  puts display_row
+end

b0ec25da49515eef02849ded42991bd9355d47e3
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-06-18 15:35:45 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,14 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+puts "   |   |   \n"
+puts "-----------\n"
+puts "   |   |   \n"
+puts "-----------\n"
+puts "   |   |   \n"
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, char = "x")
+end

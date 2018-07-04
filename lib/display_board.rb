c956b059734449b0aee8d75ba4e34d08ef6eb81d
Cristabelle Tumola <cristabelle.tumola@gmail.com>
2018-07-04 00:44:08 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,7 @@
 def display_board
-002:1 >     puts "A Tic Tac Toe Board"
-003:1 >   end
+  puts "  "  " |  "  " |  " ""
+  puts "-----------"
+  puts "  "  " |  "  " |  " ""
+  puts "-----------"
+  puts "  "  " |  "  " |  " ""
+end

41554b3623d5ec5a7cb204e25e3fbe7b848f8f58
Jordan Rumeld <jordanrumeld@me.com>
2018-07-05 19:44:30 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  puts cell = "   |   |   "
+  puts row1 = "-----------"
+  puts cell2 = "   |   |   "
+  puts row2 = "-----------"
+  puts cell3 = "   |   |   "
+end

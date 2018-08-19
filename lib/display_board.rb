496e7c378cb86e857b0650ad4b049705db7f1111
Allison Tearjen <amtearjen@ucdavis.edu>
2018-08-19 19:58:31 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,8 @@
 # Define a method print_board that prints a 3x3 Tic Tac Toe Board
-print("   |   |   ")
-print("-----------")
-print("   |   |   ")
-print("-----------")
-print("   |   |   ")
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end

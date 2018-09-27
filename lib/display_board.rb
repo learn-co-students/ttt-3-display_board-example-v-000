d4c2caceec05666de9139b8ee8429c1bc76d2b1f
langan.kg Langan <langan.code@gmail.com>
2018-09-27 19:00:37 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,14 +1,12 @@
-ea# Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def  display board
-"   |   |   "    
-"------------"
-"   |   |   "
-"------------"
-"   |   |   "
+# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def  display_board
+  
+puts "   |   |   "    
+puts "------------"
+puts "   |   |   "
+puts "------------"
+puts "   |   |   "
 end
-
-learn submit_tag
-
 
 
 

6284e7187106027d2de724999679703b46cc905a
langan.kg Langan <langan.code@gmail.com>
2019-02-26 15:06:00 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 s# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+=======
+# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+>>>>>>> 7598eb18f609d95389df86c4517b3893211c17cb
 def display_board
   
 puts "   |   |   "    
@@ -6,5 +10,10 @@ puts "------------"
 puts "   |   |   "
 puts "------------"
 puts "   |   |   "
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 7598eb18f609d95389df86c4517b3893211c17cb
 
 
